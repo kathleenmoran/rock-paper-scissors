036c663f0646bd400b27bfa56f5dc149f5981d62
Kathleen Moran <kathleenmoran10@gmail.com>
2022-03-29 18:07:54 -0400

    Add functionality to play a single round

--- play-game.js
+++ play-game.js
@@ -1,5 +1,25 @@
 function computerPlay() {
-    let moves = ['Rock', 'Paper', 'Scissors']
-    let randomIndex = Math.floor(Math.random() * moves.length)
-    return moves[randomIndex]
+    let moves = ['rock', 'paper', 'scissors'];
+    let randomIndex = Math.floor(Math.random() * moves.length);
+    return moves[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) return 'Tie!';
+
+    switch (playerSelection) {
+        case 'rock':
+            return (computerSelection === 'paper') ? 'You Lose! Paper beats Rock'
+            : 'You Win! Rock beats Scissors'
+        case 'paper':
+            return (computerSelection === 'scissors') ? 'You Lose! Scissors beats Paper'
+            : 'You Win! Paper beats Rock'
+        case 'scissors':
+            return (computerSelection == 'rock') ? 'You Lose! Rock beats Scissors'
+            : 'You Win! Scissors beats Paper'
+        default:
+            return 'The move you have entered is not valid. Please enter "rock", "paper", or "scissors".'
+    }
 }
